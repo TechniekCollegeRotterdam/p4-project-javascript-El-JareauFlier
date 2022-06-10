aff3574d35cb952e5a11dc6fed3900fc35e47dd1
El-Jareau Flier <9017738@student.tcrmbo.nl>
2022-06-10 13:53:49 +0200

    so far

---
+++ Ping pong game/game.js
@@ -0,0 +1,7 @@
+const canvas = document.getElementById("pong");
+const context = canvas.getContext('2d');
+
+context.fillStyle = 'red'
+context.fillRect(100,200,50,75);
+context.beginPath();
+context.arc(300, 350, 100, 0, Math.PI*2, false);
